f272a5f40264a2d9dc9283b60324c55782abe121
shignax <shigeki.nakai@gmail.com>
2016-12-08 15:44:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# IS01Keyboard
+
+IS01にインストールされたAndroid 4.1.2（Jelly Bean）、通称JB on IS01のキーマップを修正するUser-installable keymapsです。
+主に以下を修正しています
+
+- 「文字」キーを「全角/半角」キーにマッピング
+- 「右Shift」を「左Ctrl」キーにマッピング
+- 「Shift + 1」で「\」
+- 「Shift + 2」で「|」
---
+++ app/src/main/java/xyz/nax_9/is01keyboard/InputDeviceReceiver.java
@@ -0,0 +1,15 @@
+package xyz.nax_9.is01keyboard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by nax9 on 2016/12/08.
+ */
+public class InputDeviceReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Nothing to do at this time.
+    }
+}
